3863cdf3097e525478dc99793bcabd8e9b3b630e
Benjamin Picard <benpicar@k2r1p4.42mulhouse.fr>
2026-01-20 19:47:41 +0100

    update ai algo for win at the end

--- srcs/game/Ai.c
+++ srcs/game/Ai.c
@@ -6,7 +6,7 @@
 /*   By: benpicar <benpicar@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/19 17:22:27 by benpicar          #+#    #+#             */
-/*   Updated: 2026/01/20 11:59:07 by benpicar         ###   ########.fr       */
+/*   Updated: 2026/01/20 19:45:55 by benpicar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -87,34 +87,104 @@ inline void	ft_player_move(t_vector *tab)
 /**
  * @brief	AI move using a simple strategy based on modulo 4.
  * 
- * This function makes the AI remove matches from the last non-empty line
- * of the game board. The AI aims to leave the opponent in a losing position
- * by ensuring that the number of matches left is a multiple of 4.
+ * This function determines the AI's move by analyzing the current state
+ * of the game board. It uses a strategy based on the modulo 4 of the
+ * number of matches in the current line and the next line to decide how
+ * many matches to remove.
  * 
  * @param	tab	The game board represented as a vector.
  * @return	int	Number of matches removed by the AI.
  */
 inline int	ft_ai_move(t_vector *tab)
 {
-	int		line, matches, current_value;
+	int		line, matches = 0, current_value;
 	bool	next_line_is_mod_4;
 
 	line = tab->index - 1;
 	current_value = ((int *)tab->buf)[line];
 	
-	next_line_is_mod_4 = false;
-	if (line > 0 && ((int *)tab->buf)[line - 1] % 4 == 0)
-		next_line_is_mod_4 = true;
-	
-	if (next_line_is_mod_4 && tab->index > 1 && current_value <= 3)
-		matches = current_value;
+	if (tab->index == 2)
+	{
+		int next_line_value = ((int *)tab->buf)[line - 1];
+		
+		if (next_line_value > 5 && (next_line_value - 5) % 4 != 0)
+		{
+			if (current_value > 5)
+			{
+				matches = (current_value - 5) % 4;
+				if (matches == 0)
+					matches = 1;
+			}
+			else if (current_value == 5) // perdant
+				matches = 1;
+			else
+				matches = current_value - 1;
+		}
+		else if (next_line_value == 5 || (next_line_value - 5) % 4 == 0)
+		{
+			matches = current_value % 4;
+			if (matches == 0)
+				matches = 1;
+			if (matches > 3)
+				matches = 3;
+		}
+		else
+		{
+			if (next_line_value == 1)
+			{
+				matches = current_value % 4;
+				if (matches == 0)
+					matches = 1;
+				if (matches > 3)
+					matches = 3;
+			}
+			else if (next_line_value >= 2 && next_line_value <= 4)
+			{
+				if (current_value > 5)
+				{
+					matches = (current_value - 5) % 4;
+					if (matches == 0)
+						matches = 1;
+				}
+				else if (current_value == 5) // perdant
+					matches = 1;
+				else
+					matches = current_value - 1;
+			}
+		}
+	}
+	else if (tab->index == 1)
+	{
+		if (current_value > 5)
+		{
+			matches = (current_value - 5) % 4;
+			if (matches == 0)
+				matches = 1;
+		}
+		else
+		{
+			if (current_value == 5) // perdant
+				matches = 1;
+			else
+				matches = current_value - 1;
+		}
+	}
 	else
 	{
-		matches = current_value % 4;
-		if (matches == 0)
-			matches = 1;
-		if (matches >= current_value && current_value > 1)
-			matches = current_value - 1;
+		next_line_is_mod_4 = false;
+		if (line > 0 && ((int *)tab->buf)[line - 1] % 4 == 0)
+			next_line_is_mod_4 = true;
+		
+		if (next_line_is_mod_4 && tab->index > 1 && current_value <= 3)
+			matches = current_value;
+		else
+		{
+			matches = current_value % 4;
+			if (matches == 0)
+				matches = 1;
+			if (matches >= current_value && current_value > 1)
+				matches = current_value - 1;
+		}
 	}
 	
 	((int *)tab->buf)[line] -= matches;
